361a95bd5fcc5b26fc43f6a144b2dabc702fa6bc
asifmansoor <amansoor@amansoor.Ingenuity.local>
2015-09-29 14:49:29 +0530

    added dice files and baseanimation class

--- frameworks/boardgamesgdk/core/src/main/java/com/logicanvas/frameworks/boardgamesgdk/core/config/BasicGameConfiguration.java
+++ frameworks/boardgamesgdk/core/src/main/java/com/logicanvas/frameworks/boardgamesgdk/core/config/BasicGameConfiguration.java
@@ -6,4 +6,6 @@ package com.logicanvas.frameworks.boardgamesgdk.core.config;
 public class BasicGameConfiguration {
     public static boolean DEBUG = false;
     public static int NO_OF_PLAYERS = 4;
+    public static final boolean RIG_GAME = false;
+    public static final int[] rigList = {6,1,1,6,6,1,1,1,5,1,1,1,5,1,1,1,36,1,1,1,5,1,1,1};
 }
